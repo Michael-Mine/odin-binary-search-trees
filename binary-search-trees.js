ce3d62f99412f278740ee475e5b0fa241caa580c
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2025-08-12 18:34:21 +0100

    Add delete function

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -44,8 +44,6 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 };
 
 function insert(root, value) {
-  //   let root = Node(value);
-
   if (root === null) return Node(value);
 
   if (root.data === value) return root;
@@ -60,8 +58,42 @@ function insert(root, value) {
   return root;
 }
 
+function getSuccessor(curr) {
+  curr = curr.right;
+  while (curr !== null && curr.left !== null) {
+    curr = curr.left;
+  }
+  return curr;
+}
+
+function deleteItem(root, value) {
+  if (root === null) return root;
+
+  if (root.data > value) {
+    root.left = deleteItem(root.left, value);
+  } else if (root.data < value) {
+    root.right = deleteItem(root.right, value);
+  } else {
+    // If root matches with value
+
+    // Cases when root has 0 children or only right child
+    if (root.left === null) return root.right;
+
+    // Cases when root has only left child
+    if (root.right === null) return root.left;
+
+    //When both children are present
+    let succ = getSuccessor(root);
+    root.data = succ.data;
+    root.right = deleteItem(root.right, succ.data);
+  }
+  return root;
+}
+
 const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 let bst = Tree(testArray);
 prettyPrint(bst.root);
-bst = insert(bst.root, 10);
+// bst = insert(bst.root, 10);
+// prettyPrint(bst);
+bst = deleteItem(bst.root, 4);
 prettyPrint(bst);
